9b3544f4dd42a197a50e9d8e603445639dbff432
Georgina7 <nyokabigeorgina007@gmail.com>
2021-12-03 21:50:10 +0300

    Doctor and Med Admin Object Modification

--- api/classes/doctor.class.php
+++ api/classes/doctor.class.php
@@ -40,10 +40,64 @@ use ApprovableTrait;
       $this->setDoctorId($doctorId);
 
       $dbManager = new DbManager();
-      $doctorInfo = $dbManager->query(Doctor::DOC_TABLE, ["*"], User::USER_FOREIGN_ID. " = ?", [$this->id]);
+      $doctorInfo = $dbManager->query(Doctor::DOC_TABLE, ["*"], Doctor::DOC_ID. " = ?", [$this->id]);
       if($doctorInfo === false) return false;
       $this->setId($doctorInfo[User::USER_FOREIGN_ID]);
       return parent::loadUser($this->id);
+  }
+
+  /**
+   * requires the patient username and password
+   */
+  public function login(){
+    if(!isset($this->username)){
+      return Respond::NUE();
+    }
+    $this->setDoctorId(User::getIdFromUserName($this->username));
+    $this->setId($this->getUserId($this->doctorId, Doctor::DOC_ID, Doctor::DOC_TABLE));
+    return parent::login();
+  }
+
+  /**
+   * requirements: firstname, lastname, hospital and password
+   */
+  public function register(){
+    if(empty($this->firstName)) return Respond::NFNE();
+    if(empty($this->lastName)) return Respond::NLNE();
+    if(empty($this->hospital)) return Respond::NHE();
+
+    if(!Utility::checkName($this->firstName) || !Utility::checkName($this->lastName)) return Respond::UNE();
+
+    if(!Utility::checkName($this->hospital)) return Respond::UQHNE();
+
+    $response = parent::register();
+    if($response != Respond::OK()) return $response;
+
+    $dbManager = new DbManager();
+    $doctorId = $dbManager->insert(Doctor::DOC_TABLE, [User::USER_FOREIGN_ID, "hospital"],[$this->id, $this->hospital]);
+
+    if($doctorId == -1) return Respond::SQE();
+    $this->setDoctorId($doctorId);
+
+    $message = [
+      "username" => Doctor::genUserName($this->doctorId),
+      "hospital" => $this->hospital,
+      "firstName" => $this->firstName,
+      "lastName" => $this->lastName,
+      "message" => "Successfully registered Doctor"
+    ];
+
+    return Respond::makeResponse(
+      "OK",
+      json_encode($message)
+    );
+  }
+
+  /**
+   * @param int $doctorId - Doctor ID
+   */
+  public static function genUserName($doctorId){
+    return User::generateUserName($doctorId, Doctor::DOC);
   }
 
   /**
--- api/classes/medadmin.class.php
+++ api/classes/medadmin.class.php
@@ -47,6 +47,60 @@ use ApprovableTrait;
   }
 
   /**
+   * requires the patient username and password
+   */
+  public function login(){
+    if(!isset($this->username)){
+      return Respond::NUE();
+    }
+    $this->setMedAdminId(User::getIdFromUserName($this->username));
+    $this->setId($this->getUserId($this->medAdminId, MedAdmin::MED_ADMIN_ID, MedAdmin::MED_ADMIN_TABLE));
+    return parent::login();
+  }
+
+  /**
+   * requirements: firstname, lastname, hospital and password
+   */
+  public function register(){
+    if(empty($this->firstName)) return Respond::NFNE();
+    if(empty($this->lastName)) return Respond::NLNE();
+    if(empty($this->hospital)) return Respond::NHE();
+
+    if(!Utility::checkName($this->firstName) || !Utility::checkName($this->lastName)) return Respond::UNE();
+
+    if(!Utility::checkName($this->hospital)) return Respond::UQHNE();
+
+    $response = parent::register();
+    if($response != Respond::OK()) return $response;
+
+    $dbManager = new DbManager();
+    $medAdminId = $dbManager->insert(MedAdmin::MED_ADMIN_TABLE, [User::USER_FOREIGN_ID, "hospital"],[$this->id, $this->hospital]);
+
+    if($medAdminId == -1) return Respond::SQE();
+    $this->setMedAdminId($medAdminId);
+
+    $message = [
+      "username" => MedAdmin::genUserName($this->medAdminId),
+      "hospital" => $this->hospital,
+      "firstName" => $this->firstName,
+      "lastName" => $this->lastName,
+      "message" => "Successfully registered Medical Administrator"
+    ];
+
+    return Respond::makeResponse(
+      "OK",
+      json_encode($message)
+    );
+  }
+
+  /**
+   * @param int $medAdminId - Medical Administrator ID
+   */
+  public static function genUserName($medAdminId){
+    return User::generateUserName($medAdminId, MedAdmin::MDA);
+  }
+
+  /**
    * called to approve a doctor. It should only be called by the Admins
    */
   public function approve(){
